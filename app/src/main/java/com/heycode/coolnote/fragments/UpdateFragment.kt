3cfdf3ac33a932ec46a69c5cdb79fe9d3731dee7
Kishan Kr Sharma <skishan781@gmail.com>
2021-06-08 12:25:18 +0530

    Navigation implemented

---
+++ app/src/main/java/com/heycode/coolnote/fragments/UpdateFragment.kt
@@ -0,0 +1,19 @@
+package com.heycode.coolnote.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.heycode.coolnote.R
+
+class UpdateFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_update, container, false)
+    }
+}
